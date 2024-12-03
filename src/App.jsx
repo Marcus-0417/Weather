e56628af3a5f69c4daed30b7950d2fa5aebf0ee8
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-03 09:48:09 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import axios from "axios"
+import { IoUmbrella } from "react-icons/io5";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get('');
+      const {locations} = data.data.cwbopendata.dataset;
+      console.log(locations);
+    })
+  }, [])
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrap">
+        <h2>36小時天氣預報</h2>
+        {/* 一列兩欄 */}
+        <div className="row">
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card"></div>
+            {/* 標題 */}
+            <div className="title">台北市</div>
+            {/* 內容 */}
+            <div className="content">
+              {/* 一列三欄 */}
+              <div className="row2">
+                {/* 第一欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第二欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第三欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card"></div>
+            {/* 標題 */}
+            <div className="title">台北市</div>
+            {/* 內容 */}
+            <div className="content">
+              {/* 一列三欄 */}
+              <div className="row2">
+                {/* 第一欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第二欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第三欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
+
   )
 }
 
