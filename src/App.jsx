4827a819489b3232eecdf2742fe7d201a7e9f58c
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-04 11:20:18 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -35,20 +35,35 @@ function App() {
               <div className="item" key={city.locationName}>
                 <h3>{city.locationName}</h3>
                 <div className="content">
-                  {city.weatherElement[0].time.map((time) => {
+                  {city.weatherElement[0].time.map((time, index) => {
                     return (
-                      <div className="item2" key={time.startTime}>
-                        <p>{new Date(time.startTime).toLocaleString(undefined,{day:"numeric"})}</p>
+                      <div className="item2" key={index}>
+                        {/* 日期 */}
+                        <p>{new Date(time.startTime).toLocaleString(undefined, { day: "numeric" })}</p>
+                        {/* 時間區間 */}
                         <p>
-                        {new Date(time.startTime).toLocaleString(undefined,{hour:"numeric",minute:"numeric"})}<br />
-                        ~<br />
-                        {new Date(time.endTime).toLocaleString(undefined,{hour:"numeric",minute:"numeric"})}
+                          {new Date(time.startTime).toLocaleString(undefined, { hour: "numeric", minute: "numeric" })}<br />
+                          ~<br />
+                          {new Date(time.endTime).toLocaleString(undefined, { hour: "numeric", minute: "numeric" })}
                         </p>
-                        <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p>
-                        <p>陰短暫雨</p>
-                        <p>{city.weatherElement[0].elementName.Wx}</p>
-                        <p>{city.PoP}</p>
-                        <p><IoUmbrella size={15} color="blue" />30%</p>
+                        {/* 天氣圖示 */}
+                        {/* <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p> */}
+                        <p><img src={`./weatherIcon/${time.parameter.parameterName}.svg`} alt="" /></p>
+
+                        {/* 天氣圖名稱 */}
+                        <p>{time.parameter.parameterName}</p>
+
+                        {/* 溫度 */}
+                        <p>{city.weatherElement[2].time[index].parameter.parameterName}°C
+                          ~
+                          {city.weatherElement[1].time[index].parameter.parameterName}°C</p>
+
+                        {/* 天氣概況 */}
+                        <p>{city.weatherElement[3].time[index].parameter.parameterName}</p>
+
+                        {/* 降雨機率 */}
+                        <p><IoUmbrella size={15} color="blue" />{city.weatherElement[4].time[index].parameter.parameterName}%</p>
+
                       </div>
                     )
                   })}
@@ -61,8 +76,6 @@ function App() {
                     </p>
                     <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p>
                     <p>陰短暫雨</p>
-                    <p>{city.weatherElement[0].elementName.Wx}</p>
-                    <p>{city.PoP}</p>
                     <p><IoUmbrella size={15} color="blue" />30%</p>
                   </div> */}
                 </div>
