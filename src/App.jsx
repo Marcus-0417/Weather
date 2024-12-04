6c5633d3054ee5476290cc059bc1b296c0afd38d
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-04 10:31:55 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import { IoUmbrella } from "react-icons/io5";
-import { ShowCity } from "./Components/ShowCity"
 
 
 function App() {
+  const [weatherList, setWeatherList] = useState([]);
 
-  (async () => {
-    // 本地端要在web server伺服器上才可執行
-    // json的連結必須是公開網址下的來源
-    const data = await axios.get('https://marcus-0417.github.io/Weather/json/F-C0032-001.json');
-    // console.log(data);
-    const { location } = data.data.cwaopendata.dataset;
-    console.log(location);
+  useEffect(() => {
 
-    // locationName => 縣市名稱
-    // elementName => Wx => 天氣概況
-    // elementName => PoP => 降雨機率
+    (async () => {
+      // 本地端要在web server伺服器上才可執行
+      // json的連結必須是公開網址下的來源
+      const data = await axios.get('https://marcus-0417.github.io/Weather/json/F-C0032-001.json');
+      // console.log(data);
+      const { location } = data.data.cwaopendata.dataset;
+      console.log(location);
+      setWeatherList(location);
 
-  })()
+      // locationName => 縣市名稱
+      // elementName => Wx => 天氣概況
+      // elementName => PoP => 降雨機率
+
+    })()
+  }, [])
 
 
   return (
-
-    <>
-      <div className="wrap">
-        <h2>36小時天氣預報</h2>
-        <ShowCity />
-      </div>
-    </>
+    <div className="wrap">
+      <h2>36小時天氣預報</h2>
+      <div className="container">
+        {
+          weatherList.map((city) => {
+            return (
+              <div className="item" key={city.locationName}>
+                <h3>{city.locationName}</h3>
+                <div className="content">
+                  {city.weatherElement[0].time.map((time) => {
+                    return (
+                      <div className="item2" key={time.startTime}>
+                        <p>{new Date(time.startTime).toLocaleString(undefined,{day:"numeric"})}</p>
+                        <p>
+                        {new Date(time.startTime).toLocaleString(undefined,{hour:"numeric",minute:"numeric"})}<br />
+                        ~<br />
+                        {new Date(time.endTime).toLocaleString(undefined,{hour:"numeric",minute:"numeric"})}
+                        </p>
+                        <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p>
+                        <p>陰短暫雨</p>
+                        <p>{city.weatherElement[0].elementName.Wx}</p>
+                        <p>{city.PoP}</p>
+                        <p><IoUmbrella size={15} color="blue" />30%</p>
+                      </div>
+                    )
+                  })}
+                  {/* <div className="item2">
+                    <p>{new Date(city.weatherElement[0].time[0].startTime).toLocaleString(undefined)}</p>
+                    <p>
+                      上午6:00<br />
+                      ~<br />
+                      下午6:00
+                    </p>
+                    <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p>
+                    <p>陰短暫雨</p>
+                    <p>{city.weatherElement[0].elementName.Wx}</p>
+                    <p>{city.PoP}</p>
+                    <p><IoUmbrella size={15} color="blue" />30%</p>
+                  </div> */}
+                </div>
+              </div>
+            )
+          })
+        }
+      </div >
+    </div >
   )
 }
 export default App
